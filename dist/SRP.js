8ef807faf748e1309352467d0ee9eea87368db2e
tylerswann <72543910051ALSie1234%%##>
2020-10-19 15:05:49 -0400

    Added license to file headers

--- dist/SRP.js
+++ dist/SRP.js
@@ -1,8 +1,26 @@
 "use strict";
 /*
- * Copyright (C) 2019-2020 Tyler Swann - All Rights Reserved
- * Unauthorized copying of this file, via any medium is strictly prohibited
- * Proprietary and confidential
+ * MIT License
+ *
+ * Copyright (c) 2020 Tyler Swann
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
  * Written by Tyler Swann <tyler.swann94@gmail.com>
  * on 09/02/2020 at 10:36 AM
  */
@@ -11,13 +29,6 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.HASH = exports.BITS = exports.PAD = exports.CONCAT = exports.BinaryNumber = void 0;
-/*
- * Copyright (C) 2019-2020 Tyler Swann - All Rights Reserved
- * Unauthorized copying of this file, via any medium is strictly prohibited
- * Proprietary and confidential
- * Written by Tyler Swann <tyler.swann94@gmail.com>
- * on 08/19/2020 at 1:58 PM
- */
 const sjcl_1 = __importDefault(require("../lib/sjcl"));
 const buffer_1 = require("buffer/");
 const bignumber_js_1 = require("bignumber.js");
--- dist/SRPSession.js
+++ dist/SRPSession.js
@@ -1,4 +1,29 @@
 "use strict";
+/*
+ * MIT License
+ *
+ * Copyright (c) 2020 Tyler Swann
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ * Written by Tyler Swann <tyler.swann94@gmail.com>
+ * on 09/08/2020 at 5:19 PM
+ */
 var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
     if (k2 === undefined) k2 = k;
     Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
@@ -20,13 +45,6 @@ var __importStar = (this && this.__importStar) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.SRPServerSession = void 0;
-/*
- * Copyright (C) 2019-2020 Tyler Swann - All Rights Reserved
- * Unauthorized copying of this file, via any medium is strictly prohibited
- * Proprietary and confidential
- * Written by Tyler Swann <tyler.swann94@gmail.com>
- * on 09/08/2020 at 5:19 PM
- */
 const SRP_1 = __importStar(require("./SRP"));
 class SRPClientSession {
     constructor(password, model) {
